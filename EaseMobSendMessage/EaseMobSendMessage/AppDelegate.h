f2683d9f8536750d39735feb910a116c157999ee
ARI <912760690@qq.com>
2018-03-01 14:22:00 +0800

    提交初始化工程

---
+++ EaseMobSendMessage/EaseMobSendMessage/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  EaseMobSendMessage
+//
+//  Created by yang on 2018/3/1.
+//  Copyright © 2018年 223studio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ EaseMobSendMessage/EaseMobSendMessage/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  EaseMobSendMessage
+//
+//  Created by yang on 2018/3/1.
+//  Copyright © 2018年 223studio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
